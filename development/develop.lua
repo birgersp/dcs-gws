612aeb96f4e105ff99232adbd77bef5a0bf78f2e
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-09-30 09:51:48 +0200

    Removed lua test script, moved development-specific script

--- development/develop.lua
+++ development/develop.lua
@@ -1,5 +1,4 @@
--- Script used for mission development
--- Before mission deployment, remember to add the scripts to the mission
+-- Script used for testing utilities during development
 --devInitialized = false
 if (devInitialized == nil or devInitialized == false) then
   assert(loadfile([[C:\Users\birge\Workspace\dcs-bajas\bajas.lua]]))()
