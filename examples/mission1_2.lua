1c1632a3e067ce4e2bc5525a32edaf90411f3ddb
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-10-03 00:10:47 +0200

    Simplified example mission1_2

--- examples/mission1_2.lua
+++ examples/mission1_2.lua
@@ -13,99 +13,70 @@ local function reinforceOverrideSpawnNames(setup, tempSpawnNames)
   setup.spawnNames = spawnNamesCopy
 end
 
-local zoneNames = {
-  combatZone1 = "",
-  combatZone2 = "",
-  combatZone3 = "",
-  combatZone4 = "",
-  combatZone5 = "",
-  combatZone6 = "",
-  combatZone7 = "",
-
-  spawnB10 = "",
-  spawnB11 = "",
-  spawnB12 = "",
-
-  spawnB20 = "",
-  spawnB21 = "",
-  spawnB22 = "",
-  spawnB23 = "",
-  spawnB24 = "",
-
-  spawnR10 = "",
-  spawnR11 = "",
-  spawnR12 = "",
-
-  spawnR20 = "",
-  spawnR21 = "",
-  spawnR22 = "",
-  spawnR23 = "",
-}
-
-for i,v in pairs(zoneNames) do
-  zoneNames[i] = i
-end
-
 local rsBuilder = bajas.RSBuilder:new()
 rsBuilder:timeInterval(MIN_REINFORCEMENT_T)
 
+-- USA units
+
 rsBuilder:country(country.id.USA)
 
-rsBuilder:initialSpawns({zoneNames.spawnB20})
-rsBuilder:spawnNames({zoneNames.spawnB21, zoneNames.spawnB22})
+rsBuilder:initialSpawns({"spawnB20"})
+rsBuilder:spawnNames({"spawnB21", "spawnB22"})
 
-rsBuilder:destinationName(zoneNames.combatZone1)
-
-rsBuilder:unitType(vehicles.Tanks.M1_Abrams):unitCount(4):register()
-rsBuilder:unitType(vehicles.IFV.BRDM2):unitCount(2):register()
-rsBuilder:unitType(vehicles.IFV.LAV25):unitCount(2):register()
-
-rsBuilder:destinationName(zoneNames.combatZone3)
+rsBuilder:destinationName("combatZone1")
 
 rsBuilder:unitType(vehicles.Tanks.M1_Abrams):unitCount(4):register()
 rsBuilder:unitType(vehicles.IFV.BRDM2):unitCount(2):register()
 rsBuilder:unitType(vehicles.IFV.LAV25):unitCount(2):register()
 
-rsBuilder:initialSpawns({zoneNames.spawnB24})
-rsBuilder:destinationName(zoneNames.combatZone5)
+rsBuilder:destinationName("combatZone3")
+
+rsBuilder:unitType(vehicles.Tanks.M1_Abrams):unitCount(4):register()
+rsBuilder:unitType(vehicles.IFV.BRDM2):unitCount(2):register()
+rsBuilder:unitType(vehicles.IFV.LAV25):unitCount(2):register()
+
+rsBuilder:initialSpawns({"spawnB24"})
+rsBuilder:destinationName("combatZone5")
 
 rsBuilder:unitType(vehicles.IFV.BMP3):unitCount(4):register()
 rsBuilder:unitType(vehicles.IFV.BRDM2):unitCount(2):register()
 rsBuilder:unitType(vehicles.IFV.LAV25):unitCount(4):register()
 
-rsBuilder:spawnNames({zoneNames.spawnB21, zoneNames.spawnB23})
-rsBuilder:destinationName(zoneNames.combatZone7)
+rsBuilder:spawnNames({"spawnB21", "spawnB23"})
+rsBuilder:destinationName("combatZone7")
 
 rsBuilder:unitType(vehicles.Tanks.M1_Abrams):unitCount(4):register()
 rsBuilder:unitType(vehicles.IFV.BRDM2):unitCount(2):register()
 rsBuilder:unitType(vehicles.IFV.LAV25):unitCount(2):register()
 
--- Russian units
+-- Russia units
 
 rsBuilder:country(country.id.RUSSIA)
 
-rsBuilder:initialSpawns({zoneNames.spawnR20})
-rsBuilder:spawnNames({zoneNames.spawnR22, zoneNames.spawnR23})
-rsBuilder:destinationName(zoneNames.combatZone2)
+rsBuilder:initialSpawns({"spawnR20"})
+rsBuilder:spawnNames({"spawnR22", "spawnR23"})
+
+rsBuilder:destinationName("combatZone2")
 
 rsBuilder:unitType(vehicles.Tanks.T90):unitCount(4):register()
 rsBuilder:unitType(vehicles.IFV.BRDM2):unitCount(2):register()
 rsBuilder:unitType(vehicles.IFV.BTR80):unitCount(2):register()
 
-rsBuilder:destinationName(zoneNames.combatZone4)
+rsBuilder:destinationName("combatZone4")
 
 rsBuilder:unitType(vehicles.Tanks.T90):unitCount(4):register()
 rsBuilder:unitType(vehicles.IFV.BRDM2):unitCount(2):register()
 rsBuilder:unitType(vehicles.IFV.BTR80):unitCount(2):register()
 
-rsBuilder:spawnNames({zoneNames.spawnR21, zoneNames.spawnB22})
-rsBuilder:destinationName(zoneNames.combatZone5)
+rsBuilder:spawnNames({"spawnR21", "spawnR22"})
+
+rsBuilder:destinationName("combatZone5")
 
 rsBuilder:unitType(vehicles.IFV.BMP3):unitCount(4):register()
 rsBuilder:unitType(vehicles.IFV.BRDM2):unitCount(2):register()
 rsBuilder:unitType(vehicles.IFV.BTR80):unitCount(4):register()
 
-rsBuilder:destinationName(zoneNames.combatZone6)
+rsBuilder:destinationName("combatZone6")
 
 rsBuilder:unitType(vehicles.Tanks.T90):unitCount(4):register()
 rsBuilder:unitType(vehicles.IFV.BRDM2):unitCount(2):register()
