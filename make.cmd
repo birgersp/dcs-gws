27f2881cebbb5e5f04e8a369b974b9bf0fcfc089
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-10-06 17:09:17 +0200

    Expanded make script, now makes both main and standalone build

---
+++ make.cmd
@@ -0,0 +1,43 @@
+@echo off
+
+set output_dir=build
+set sources=bajas\bajas.lua
+set sources_standalone=%sources% mist\mist_4_3_74.lua
+set build=%output_dir%\bajas.lua
+set build_standalone=%output_dir%\bajas-standalone.lua
+
+set comment_prefix=--
+
+if not exist %output_dir% md %output_dir%
+set input=%sources%
+set output=%build%
+call:make
+set input=%sources_standalone%
+set output=%build_standalone%
+call:make
+pause
+goto:eof
+
+:make
+	echo Writing to %output%
+	
+	break>%output%
+	setlocal EnableDelayedExpansion
+	set first=1
+	for %%a in (%input%) do (
+		echo Appending %%a
+		if !first!==0 (
+			echo.>> %output%
+			echo.>> %output%
+			echo.>> %output%
+		)
+	
+		echo %comment_prefix%>>%output% %%a
+		echo.>> %output%
+		type %%a>>%output%
+		
+		set first=0
+	)
+	
+	echo Done
+goto:eof
