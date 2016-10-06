8b5c0cc72a42aaaf20a19adf68f7b6a3d10b4880
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-10-06 17:33:00 +0200

    Removed unit type definitions from main source

--- development/test.lua
+++ development/test.lua
@@ -1,15 +1,15 @@
 if initialized == nil then
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-bajas\bajas\bajas.lua]]))()
+  assert(loadfile([[C:\Users\birge\Workspace\dcs-bajas\build\bajas.lua]]))()
   
   local taskForce1 = bajas.TaskForce:new(country.id.USA,"task1", {"task1", "task2", "task3"})
-  taskForce1:addUnitSpec(2,bajas.unitTypes.vehicles.Tanks.M1_Abrams)
-  taskForce1:addUnitSpec(2,bajas.unitTypes.vehicles.Tanks.M1_Abrams)
+  taskForce1:addUnitSpec(2,unitTypes.vehicles.tanks.M1_Abrams)
+  taskForce1:addUnitSpec(2,unitTypes.vehicles.tanks.M1_Abrams)
   taskForce1:enableAdvanceInterval(300)
   taskForce1:enableReinforceInterval(60)
   
   local taskForce2 = bajas.TaskForce:new(country.id.RUSSIA,"task3", {"task3", "task2", "task1"})
-  taskForce2:addUnitSpec(2,bajas.unitTypes.vehicles.Tanks.T72B)
-  taskForce2:addUnitSpec(2,bajas.unitTypes.vehicles.Tanks.T72B)
+  taskForce2:addUnitSpec(2,unitTypes.vehicles.tanks.T72B)
+  taskForce2:addUnitSpec(2,unitTypes.vehicles.tanks.T72B)
   taskForce2:enableAdvanceInterval(300)
   taskForce2:enableReinforceInterval(60)
   
