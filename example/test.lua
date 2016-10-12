d898decd1218e41283a2645c1b8b4a8f315e305a
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-10-12 17:45:46 +0200

    Renamed namespace

--- example/test.lua
+++ example/test.lua
@@ -1,15 +1,15 @@
 if initialized == nil then
   assert(loadfile([[C:\Users\birge\Workspace\dcs-mission-intel\build\mint.lua]]))()
   
-  local taskForce2 = bajas.TaskForce:new(country.id.RUSSIA,{"spawn1"},{"task1", "task2"})
-  taskForce2:addUnitSpec(4,unitType.vehicle.tank.M1_Abrams)
+  local taskForce2 = mint.TaskForce:new(country.id.RUSSIA,{"spawn1"},{"task1", "task2"})
+  taskForce2:addUnitSpec(4,unitType.vehicle.tank.T90)
   taskForce2:enableDefault()
 
-  local taskForce1 = bajas.TaskForce:new(country.id.USA,{"spawn2"},{"task2", "task1"})
-  taskForce1:addUnitSpec(4,unitType.vehicle.tank.T90)
+  local taskForce1 = mint.TaskForce:new(country.id.USA,{"spawn2"},{"task2", "task1"})
+  taskForce1:addUnitSpec(4,unitType.vehicle.tank.M1_Abrams)
   taskForce1:enableDefault()
 
-  bajas.enableIOCEVForGroups()
+  mint.enableIOCEVForGroups()
 
   initialized = true
 end
