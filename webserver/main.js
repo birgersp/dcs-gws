d97a0c2467b7e20e3737edd809a3680fea2af690
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-10-12 18:16:27 +0200

    Added basic http server functionality

--- webserver/main.js
+++ webserver/main.js
@@ -1,7 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Lets require/import the HTTP module
+var http = require('http');
+var fs = require('fs');
 
+//Lets define a port we want to listen to
+var PORT=8080; 
 
+var dispatcher = require('httpdispatcher');
+
+//For all your static (js/css/images/etc.) set the directory name (relative path).
+dispatcher.setStatic('public');
+
+//Lets use our dispatcher
+function handleRequest(req, res){
+    fs.readFile('public/index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
