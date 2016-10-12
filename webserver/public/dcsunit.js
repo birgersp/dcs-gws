a7b2dd75985cde4484aba8b69492940c0fe03e97
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-10-12 19:43:14 +0200

    Added coalition member to dcs unit type

--- webserver/public/dcsunit.js
+++ webserver/public/dcsunit.js
@@ -1,6 +1,7 @@
 function DCSUnit() {
     this.x = 0;
     this.z = 0;
+    this.coalition = DCSUnit.coalition.RED;
     this.type = DCSUnit.types.TANK;
 }
 
@@ -8,4 +9,10 @@ function DCSUnit() {
 DCSUnit.types = {
     TANK: "TANK",
     IFV: "IFV"
+};
+
+DCSUnit.coalition = {
+    NEUTRAL: "NEUTRAL",
+    RED: "RED",
+    BLUE: "BLUE"
 };
