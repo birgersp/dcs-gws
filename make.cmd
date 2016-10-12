501ee77b0fb74ec090faef0e65e7da5238117ead
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-10-12 18:00:04 +0200

    Moved core script til "lua" folder

--- make.cmd
+++ make.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 set output_dir=build
-set sources=mint\core.lua unit-types\unittypes.lua
+set sources=lua\core.lua unit-types\unittypes.lua
 set sources_standalone=%sources% mist\mist_4_3_74.lua
 set build=%output_dir%\mint.lua
 set build_standalone=%output_dir%\mint-standalone.lua
