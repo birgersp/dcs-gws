e89b08860b0159aa708e370682c188b729be92bc
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-10-14 13:23:22 +0200

    Added example mission inclusion in build script

--- make.cmd
+++ make.cmd
@@ -3,6 +3,7 @@
 set output_dir=build
 set sources=lua\core.lua unit-types\unittypes.lua
 set sources_standalone=%sources% mist\mist_4_3_74.lua
+set include=example\mission.lua
 set build=%output_dir%\mint.lua
 set build_standalone=%output_dir%\mint-standalone.lua
 
@@ -16,6 +17,12 @@ call:make
 set input=%sources_standalone%
 set output=%build_standalone%
 call:make
+
+for %%a in (%include%) do (
+	echo Including %%a
+	copy %%a %output_dir%
+)
+
 goto:eof
 
 :make
