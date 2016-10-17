df8bd3bbd1373fcce5c42aff7cb59785a4b2ba4c
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-10-17 10:12:50 +0200

    Implemented new task for instantiation method to test mission script

--- test/test.lua
+++ test/test.lua
@@ -3,28 +3,28 @@ if not initialized then
   autogft.debugMode = true
 
 
-  local usTaskForce = autogft.TaskForce:new(
-    country.id.USA, {"SPAWN1", "SPAWN2"}, {"Combat1", "Combat2", "Combat3"})
-  usTaskForce:addUnitSpec(4, "M-1 Abrams")
-  usTaskForce:addUnitSpec(4, "M-1 Abrams")
-  usTaskForce:addUnitSpec(4, "LAV-25")
-  usTaskForce:respawn()
-  usTaskForce:enableObjectiveUpdateTimer(120)
-  usTaskForce:enableRespawnTimer(300, 1200)
-  
-  local russiaTaskForce1 = autogft.TaskForce:new(
-    country.id.RUSSIA, {"SPAWN3"}, {"Combat3", "Combat2", "Combat1"})
-  russiaTaskForce1:addUnitSpec(4, "T-90")
-  russiaTaskForce1:respawn()
-  russiaTaskForce1:enableObjectiveUpdateTimer(120)
-  russiaTaskForce1:enableRespawnTimer(300)
-  
-  local russiaTaskForce2 = autogft.TaskForce:new(
-    country.id.RUSSIA, {"Combat2"}, {"Combat2"})
-  russiaTaskForce2:addUnitSpec(2, "BMP-2")
-  russiaTaskForce2:respawn()
-  russiaTaskForce2:enableObjectiveUpdateTimer(250)
-  russiaTaskForce2:enableRespawnTimer(120, 600)
+  autogft.TaskForce
+    :new(country.id.USA, {"SPAWN1", "SPAWN2"}, {"Combat1", "Combat2", "Combat3"})
+    :addUnitSpec(4, "M-1 Abrams")
+    :addUnitSpec(4, "M-1 Abrams")
+    :addUnitSpec(4, "LAV-25")
+    :respawn()
+    :enableObjectiveUpdateTimer(120)
+    :enableRespawnTimer(300, 1200)
+
+  autogft.TaskForce
+    :new(country.id.RUSSIA, {"SPAWN3"}, {"Combat3", "Combat2", "Combat1"})
+    :addUnitSpec(4, "T-90")
+    :respawn()
+    :enableObjectiveUpdateTimer(120)
+    :enableRespawnTimer(300)
+
+  autogft.TaskForce
+    :new(country.id.RUSSIA, {"Combat2"}, {"Combat2"})
+    :addUnitSpec(2, "BMP-2")
+    :respawn()
+    :enableObjectiveUpdateTimer(250)
+    :enableRespawnTimer(120, 600)
 
 
   autogft.enableIOCEV()
