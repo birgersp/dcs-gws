e3a94144bd2f1b2d537065ce8267cfe8128f7dec
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-12-15 18:18:43 +0100

    Updated example

--- example/example.lua
+++ example/example.lua
@@ -1,9 +1,19 @@
 
--- Autonomous Ground Force Tasking script, example usage
--- (Lines beginning with "--" does not affect the mission)
+-- Autonomous Ground Force Tasking example script
+-- (Lines beginning with "--" are comments and does not affect the code)
+-- (Remember to re-load this script to your mission after you have edited it)
+-- Comments above code blocks explain what the code is doing
 
 
--- Create a task force of US units, set "SPAWN1"/"SPAWN2" as the spawn zone and "Combat" "1", "2" and "3" as objectives
+
+-- 1. Creates a task force of US units
+-- 2. Adds zones "SPAWN1" and "SPAWN2" as base zones (for spawning or staging)
+-- 3. Adds zones "Combat1", "Combat" and "Combat3" as target zones. "Combat1" will be the first target
+-- 4. Adds two groups of M1-Abrams, and a group of 4 LAV-25s
+-- 5. Respawns the task force (at the base zones)
+-- 6. Sets a "target update timer" which a time interval of how often orders will be updated
+-- 7. Sets a "respawn timer" which is a time interval of how often the task force will be reinforced by spawning new units
+
 autogft.TaskForce:new()
   :setCountry(country.id.USA)
   :addBaseZone("SPAWN1")
@@ -11,21 +21,19 @@ autogft.TaskForce:new()
   :addTargetZone("Combat1")
   :addTargetZone("Combat2")
   :addTargetZone("Combat3")
-  -- Add 4 M-1 Abrams to the task force
   :addUnitSpec(4, "M-1 Abrams")
-  -- Add more units ...
   :addUnitSpec(3, "M-1 Abrams")
-  -- Add some LAV-25's aswell
   :addUnitSpec(4, "LAV-25")
-  -- Spawn the task force once (the "true" parameter specifies to spawn the units)
   :respawn()
-  -- Enable automatic objective update every 120 seconds
   :setTargetUpdateTimer(120)
-  -- Enable automatic re-spawning of destroyed units every 300 seconds, for a total of 1200 seconds
   :setRespawnTimer(300)
 
 
--- Create a task force of Russian units, with some different spawn zone and objectives
+
+-- Same as the previous code, but this time for some russian units and with a different base 
+-- Note this task force will not respawn, but "re-stage" instead
+-- This means the task force will only be reinforced by units that are located in the base prior to reinforcing 
+
 autogft.TaskForce:new()
   :setCountry(country.id.RUSSIA)
   :addBaseZone("STAGING1")
@@ -33,11 +41,12 @@ autogft.TaskForce:new()
   :addTargetZone("Combat2")
   :addTargetZone("Combat1")
   :addUnitSpec(4, "T-90")
-  -- (Note there is no "true" parameter in this reinforcement, so it will only use pre-spawned units)
   :setTargetUpdateTimer(120)
-  -- (Note there is no second time value in this timer, so this re-spawning will go on forever)
   :setRestageTimer(10)
+
+
 
 -- Enable "Intel On Closest Enemy Vehicle" radio command option (F10) for players
 -- Note: This is enabled 30 seconds after player group is activated
+
 autogft.enableIOCEV()
