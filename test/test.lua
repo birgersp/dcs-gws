f09822f6be56fd5285cebc179632d6ce2a52b3bb
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-12-29 18:15:05 +0100

    Fixed reference in test script

--- test/test.lua
+++ test/test.lua
@@ -2,7 +2,7 @@
 if not initialized then
 
   assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\autogft-standalone.lua]]))()
-  autogft.debugMode = true
+  autogft_debugMode = true
   
   assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\example.lua]]))()
 
