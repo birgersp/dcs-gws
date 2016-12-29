dad65da1933fb3a51c881b673f57321db4c4ab26
Birger Skogeng Pedersen <birger.sp@gmail.com>
2016-12-29 14:42:51 +0100

    Moved IOCEV code out of core

--- autogft/core.lua
+++ autogft/core.lua
@@ -1,11 +1,5 @@
 ---
--- @module autogft
-
--- Namespace declaration
-
-autogft = {
-  TaskForce = autogft_TaskForce
-}
+-- @module autogft_core
 
 -- Constants
 
@@ -121,83 +115,6 @@ function autogft_radToCardinalDir(rad)
 end
 
 ---
--- This function might be computationally expensive
--- @param DCSUnit#Unit unit
--- @param #number radius
--- @return #autogft_UnitCluster
-function autogft_getFriendlyVehiclesWithin(unit, radius)
-  local coalitionString
-  if unit:getCoalition() == coalition.side.BLUE then
-    coalitionString = "[blue]"
-  else
-    coalitionString  = "[red]"
-  end
-  local unitTableStr = coalitionString .. '[vehicle]'
-  local units = mist.makeUnitTable({ unitTableStr })
-
-  local addedVehiclesNames = {unit:getName()}
-  local minPos = unit:getPosition().p
-  local maxPos = unit:getPosition().p
-
-  ---
-  -- @param #list list
-  -- @param value
-  local function contains(list, value)
-    for i = 1, #list do
-      if list[i] == value then
-        return true
-      end
-    end
-    return false
-  end
-
-  ---
-  -- @param DCSUnit#Unit unit
-  local function addUnit(unit)
-    local pos = unit:getPosition().p
-    if pos.x < minPos.x then minPos.x = pos.x end
-    if pos.z < minPos.z then minPos.z = pos.z end
-    if pos.x > maxPos.x then maxPos.x = pos.x end
-    if pos.z > maxPos.z then maxPos.z = pos.z end
-    addedVehiclesNames[#addedVehiclesNames + 1] = unit:getName()
-  end
-
-  ---
-  -- @param DCSUnit#Unit unit
-  local function vehiclesWithinRecurse(targetUnit)
-    for i = 1, #units do
-      local nextUnit = Unit.getByName(units[i])
-      if nextUnit then
-        if nextUnit:getID() == targetUnit:getID() == false then
-          if autogft_getDistanceBetween(targetUnit:getPosition().p, nextUnit:getPosition().p) <= radius then
-            if contains(addedVehiclesNames, nextUnit:getName()) == false then
-              addUnit(nextUnit)
-              vehiclesWithinRecurse(nextUnit)
-            end
-          end
-        end
-      end
-    end
-  end
-
-  vehiclesWithinRecurse(unit)
-
-  local dx = maxPos.x - minPos.x
-  local dz = maxPos.z - minPos.z
-
-  local midPoint = { -- 3D to 2D conversion implemented
-    x = minPos.x + dx / 2,
-    y = minPos.z + dz / 2
-  }
-
-  local result = autogft_UnitCluster:new()
-  result.unitNames = addedVehiclesNames
-  result.midPoint = midPoint
-  return result
-
-end
-
----
 --Prints out a message to a group, describing nearest enemy vehicles
 -- @param DCSGroup#Group group
 function autogft_informOfClosestEnemyVehicles(group)
@@ -247,72 +164,6 @@ function autogft_informOfClosestEnemyVehicles(group)
 
 end
 
-function autogft_enableIOCEV()
-
-  local enabledGroupCommands = {}
-
-  local function cleanEnabledGroupCommands()
-    local newEnabledGroupCommands = {}
-    for i = 1, #enabledGroupCommands do
-      if not Group.getByName(enabledGroupCommands[i].groupName) then
-        enabledGroupCommands[i]:disable()
-      else
-        newEnabledGroupCommands[#newEnabledGroupCommands + 1] = enabledGroupCommands[i]
-      end
-    end
-    enabledGroupCommands = newEnabledGroupCommands
-  end
-
-  ---
-  -- @param #number groupId
-  local function groupHasCommandEnabled(groupId)
-    for i = 1, #enabledGroupCommands do
-      if enabledGroupCommands[i].groupId == groupId then
-        return true
-      end
-    end
-    return false
-  end
-
-  ---
-  -- @param DCSGroup#Group group
-  local function groupHasPlayer(group)
-    local units = group:getUnits()
-    for i = 1, #units do
-      if units[i]:getPlayerName() ~= nil then return true end
-    end
-    return false
-  end
-
-  ---
-  -- @param #list<DCSGroup#Group> groups
-  local function enableForGroups(groups)
-    for i = 1, #groups do
-      local group = groups[i]
-      if groupHasPlayer(group) then
-        if not groupHasCommandEnabled(group:getID()) then
-          local function triggerCommand()
-            autogft_informOfClosestEnemyVehicles(group)
-          end
-          local groupCommand = autogft_GroupCommand:new(autogft_IOCEV_COMMAND_TEXT, group:getName(), triggerCommand)
-          groupCommand:enable()
-          enabledGroupCommands[#enabledGroupCommands + 1] = groupCommand
-        end
-      end
-    end
-  end
-
-  local function reEnablingLoop()
-    cleanEnabledGroupCommands()
-    enableForGroups(coalition.getGroups(coalition.side.RED))
-    enableForGroups(coalition.getGroups(coalition.side.BLUE))
-    autogft_scheduleFunction(reEnablingLoop, 30)
-  end
-
-  reEnablingLoop()
-
-end
-
 function autogft_debug(variable, text)
   if autogft_debugMode then
     if text then
@@ -324,6 +175,7 @@ function autogft_debug(variable, text)
 end
 
 ---
+-- @function autogft_getUnitsInZones
 -- @param #number coalitionId
 -- @param #list<#string> zoneNames
 function autogft_getUnitsInZones(coalitionId, zoneNames)
