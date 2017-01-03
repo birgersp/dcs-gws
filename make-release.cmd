8955a91ce85f584a16e8cbfa732e6c42add2849c
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-01-03 13:55:31 +0100

    Release zip and example mission names now contains version number

--- make-release.cmd
+++ make-release.cmd
@@ -1,14 +1,16 @@
+set /p version=<version.txt
+
 set example_mission="C:\Users\birge\Saved Games\DCS\Missions\autogft-example.miz"
 set build_dir=build
 set archive_dir=build-zip
-set archive_file=%archive_dir%\autogft.zip
+set archive_file=%archive_dir%\autogft-%version%.zip
 
 call make.cmd
 call make-docs.cmd
 
 copy README.md %build_dir%\README.txt
 
-copy %example_mission% %build_dir%\example.miz
+copy %example_mission% %build_dir%\example-%version%.miz
 
 md %build_dir%\docs
 copy docs %build_dir%\docs
