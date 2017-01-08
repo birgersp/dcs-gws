1c743542bfffa7f13e1711a0cd1e2df5576b1d99
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-01-08 15:27:34 +0100

    Changed test script reference

--- example/test.lua
+++ example/test.lua
@@ -3,18 +3,8 @@ if not initialized then
 
   assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\autogft-1_6_SNAP-standalone.lua]]))()
   autogft_debugMode = true
-  
-  -- assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\example.lua]]))()
 
-  autogft_TaskForce:new()
-    :setCountry(country.id.USA)
-    :addBaseZone("SPAWN1")
-    :addControlZone("Combat1")
-    :addControlZone("Combat3")
-    :addGroup(2, "M-1 Abrams")
-    :respawn()
-    :setAdvancementTimer(300)
-    :setRespawnTimer(120)
+  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\example\example.lua]]))()
 
   initialized = true
 end
