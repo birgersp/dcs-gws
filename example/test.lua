9b278359c7084438abc523da60730c14d8a6dbb7
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-01-08 11:03:35 +0100

    Modified test script to simplify test

--- example/test.lua
+++ example/test.lua
@@ -1,10 +1,20 @@
 
 if not initialized then
 
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\autogft-1_5-standalone.lua]]))()
+  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\autogft-1_6_SNAP-standalone.lua]]))()
   autogft_debugMode = true
   
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\example.lua]]))()
+  -- assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\example.lua]]))()
+
+  autogft_TaskForce:new()
+    :setCountry(country.id.USA)
+    :addBaseZone("SPAWN1")
+    :addControlZone("Combat1")
+    :addControlZone("Combat3")
+    :addGroup(2, "M-1 Abrams")
+    :respawn()
+    :setAdvancementTimer(300)
+    :setRespawnTimer(600)
 
   initialized = true
 end
