c2de35ba86e13668a584b7d9694d32d7dd70e8a1
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-01-11 23:18:54 +0100

    Changed version to 1.6

--- example/test.lua
+++ example/test.lua
@@ -1,10 +1,10 @@
 
 if not initialized then
 
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\autogft-1_6_SNAP-standalone.lua]]))()
+  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\autogft-1_6-standalone.lua]]))()
   autogft_debugMode = true
 
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\example\example-simple.lua]]))()
+  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\example\example.lua]]))()
 
   initialized = true
 end
