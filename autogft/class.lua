616429fe43c7a1e7145432234e7b89597fd6c848
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-01-18 10:14:00 +0100

    Added class emulation module

---
+++ autogft/class.lua
@@ -0,0 +1,35 @@
+
+---
+-- Class emulation.
+-- @module Class
+
+---
+-- A template for creating autogft objects.
+-- @type Class
+autogft_Class = {}
+
+---
+-- Creates a new class.
+-- @param #Class self
+-- @return #Class
+function autogft_Class:create()
+  return setmetatable({}, {__index = autogft_Class})
+end
+
+---
+-- Creates a subclass of this class.
+-- @param #Class self
+-- @return #Class
+function autogft_Class:extend()
+  local class = autogft_Class:new()
+  setmetatable(class, {__index = self})
+  return class
+end
+
+---
+-- Instantiates this class.
+-- @param #Class self
+-- @return #table
+function autogft_Class:new()
+  return setmetatable({}, {__index = self})
+end
