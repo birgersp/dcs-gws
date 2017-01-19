a73f6f73479386812adb325c6efd2bd2e266ce41
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-01-19 17:39:38 +0100

    Fixed class example

--- examples/class.lua
+++ examples/class.lua
@@ -1,24 +1,55 @@
 
+---
+-- Showing how to use class emulation
+-- (Do not distribute in autogft release)
+
+
+
+package.path = package.path .. ";../autogft/?.lua"
+require("class")
+
+
+
+---
+-- @type Mammal
+-- @extends Class#Class
+-- @field #string name
 Mammal = autogft_Class:create()
 
+---
+-- @param #Mammal self
+-- @param #string name
+-- @return #Mammal
 function Mammal:new(name)
   self = Mammal:createInstance()
   self.name = name
   return self
 end
 
+---
+-- @param #Mammal self
 function Mammal:print()
-  print("Mammal name: " .. name)
+  print("Mammal name: " .. self.name)
 end
 
 
 
+---
+-- @type Person
+-- @extends #Mammal
 Person = Mammal:extend()
 
 
 
+---
+-- @type Dog
+-- @extends #Mammal
 Dog = Mammal:extend()
 
+---
+-- @param #Dog self
+-- @field #string name
+-- @field #Person owner
 function Dog:new(name, owner)
   local animal = Mammal:new(name)
   self = Dog:createInstance(animal)
@@ -26,6 +57,8 @@ function Dog:new(name, owner)
   return self
 end
 
+---
+-- @param #Dog self
 function Dog:print()
   Mammal.print(self)
   print("Owner name: " .. self.owner.name)
@@ -33,8 +66,11 @@ end
 
 
 
-local person = Person:new("Charlie Brown")
-person:print()
+local function test()
+  local person = Person:new("Charlie Brown")
+  person:print()
 
-local dog = Dog:new("Snoopy", person)
-dog:print()
+  local dog = Dog:new("Snoopy", person)
+  dog:print()
+end
+test()
