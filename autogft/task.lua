5a31b32056ac7e8ee1e897bf2e73b7ca68fa8db1
Birger Skogeng Pedersen <birgersp@gmail.com>
2017-01-23 15:16:29 +0100

    Added CaptureTask class

--- autogft/task.lua
+++ autogft/task.lua
@@ -4,7 +4,7 @@
 ---
 -- @type Task
 -- @extends class#Class
--- @field #boolean cleared
+-- @field #boolean accomplished
 -- @field #boolean useRoads
 -- @field #number speed
 autogft_Task = autogft_Class:create()
@@ -14,10 +14,17 @@ autogft_Task = autogft_Class:create()
 -- @return #Task
 function autogft_Task:new()
   self = self:createInstance()
-  self.cleared = false
+  self.accomplished = false
   self.useRoads = false
   self.speed = 100
   return self
+end
+
+---
+-- @param #Task self
+-- @return #boolean
+function autogft_Task:isAccomplished()
+  return self.accomplished
 end
 
 ---
@@ -34,4 +41,77 @@ function autogft_ZoneTask:new(zoneName)
   autogft.assertZoneExists(zoneName)
   self.zone = trigger.misc.getZone(zoneName)
   return self
+end
+
+---
+-- @type CaptureTask
+-- @extends #ZoneTask
+-- @field #number targetCoalition
+autogft_CaptureTask = autogft_ZoneTask:extend()
+
+---
+-- @param #CaptureTask self
+-- @param #number targetCoalition
+-- @return #CaptureTask
+function autogft_CaptureTask:new(zoneName, targetCoalition)
+  self = self:createInstance(autogft_ZoneTask:new(zoneName))
+  self.targetCoalition = targetCoalition
+  return self
+end
+
+---
+-- @param #CaptureTask self
+-- @param #number coalitionID
+-- @return #boolean
+function autogft_CaptureTask:hasUnitPresent(coalitionID)
+  local radiusSquared = self.zone.radius * self.zone.radius
+  local result = false
+  local groups = coalition.getGroups(coalitionId)
+  local groupIndex = 1
+  while not result and groupIndex <= #groups do
+    local units = groups[groupIndex]:getUnits()
+    local unitIndex = 1
+    while not result and unitIndex <= #units do
+      local unit = units[unitIndex]
+      local pos = unit:getPosition().p
+      local dx = self.zone.point.x - pos.x
+      local dy = self.zone.point.z - pos.z
+      if (dx*dx + dy*dy) <= radiusSquared then
+        result = true
+      end
+    end
+  end
+  return result
+end
+
+---
+-- @param #CaptureTask self
+-- @return #boolean
+function autogft_CaptureTask:hasFriendlyPresent()
+  return self:hasUnitPresent(self.targetCoalition)
+end
+
+---
+-- @param #CaptureTask self
+-- @return #boolean
+function autogft_CaptureTask:hasEnemyPresent()
+  local enemyCoalition
+  if self.targetCoalition == coalition.side.BLUE then
+    enemyCoalition = coalition.side.RED
+  else
+    enemyCoalition = coalition.side.BLUE
+  end
+  return self:hasUnitPresent(enemyCoalition)
+end
+
+---
+-- @param #CaptureTask self
+-- @return #boolean
+function autogft_CaptureTask:isAccomplished()
+  if not autogft_Task.isAccomplished(self) then
+    if self:hasFriendlyPresent() and not self:hasEnemyPresent() then
+      self.accomplished = true
+    end
+  end
+  return autogft_Task.isAccomplished(self)
 end
