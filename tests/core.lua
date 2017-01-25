a3bd49798b2a5d44d97a67337808df78cf3a87d1
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-01-25 18:00:43 +0100

    Removed docs (will be re-added)

---
+++ tests/core.lua
@@ -0,0 +1,12 @@
+
+
+package.path = package.path .. ";../autogft/?.lua"
+require("core")
+autogft.debugMode = true
+
+local obj1 = {}
+local obj2 = {}
+
+obj1.sibling = obj2
+obj2.sibling = obj1
+autogft.log(obj1)
