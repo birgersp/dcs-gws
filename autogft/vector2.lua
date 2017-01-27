ca9fae1307d63da00389279cd1691ffa4274ddd9
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-01-27 22:50:39 +0100

    Added vector2 class

---
+++ autogft/vector2.lua
@@ -0,0 +1,82 @@
+---
+-- @module Vector2
+
+---
+-- @type Vector2
+-- @extends class#Class
+autogft_Vector2 = autogft_Class:create()
+
+---
+-- @param #Vector2 self
+-- @return #Vector2
+function autogft_Vector2:new(x, y)
+  self = self:createInstance()
+  self.x = x
+  self.y = y
+  return self
+end
+
+---
+-- @param #Vector2 self
+-- @param #Vector2 vector
+-- @return #Vector2
+function autogft_Vector2:add(vector)
+  self.x = self.x + vector.x
+  self.y = self.y + vector.y
+  return self
+end
+
+---
+-- @param #Vector2 self
+-- @param #number factor
+-- @return #Vector2
+function autogft_Vector2:scale(factor)
+  self.x = self.x * factor
+  self.y = self.y * factor
+  return self
+end
+
+---
+-- @param #Vector2 self
+-- @return #Vector2
+function autogft_Vector2:getMagnitude()
+  return math.sqrt(self.x^2 + self.y^2)
+end
+
+---
+-- @param #Vector2 self
+-- @return #Vector2
+function autogft_Vector2:normalize()
+  return self:scale(1 / self:getMagnitude())
+end
+
+---
+-- @param #Vector2 self
+-- @param #Vector2 vector
+-- @return #Vector2
+function autogft_Vector2:plus(vector)
+  return autogft_Vector2:new(self.x, self.y):add(vector)
+end
+
+---
+-- @param #Vector2 self
+-- @param #number factor
+-- @return #Vector2
+function autogft_Vector2:times(factor)
+  return autogft_Vector2:new(self.x, self.y):scale(factor)
+end
+
+---
+-- @param #Vector2 self
+-- @return #Vector2
+function autogft_Vector2:copy()
+  return autogft_Vector2:new(self.x, self.y)
+end
+
+---
+-- @param #Vector2 self
+-- @param #Vector2 vector
+-- @return #Vector2
+function autogft_Vector2:minus(vector)
+  return autogft_Vector2:new(self.x, self.y):add(vector:times(-1))
+end
