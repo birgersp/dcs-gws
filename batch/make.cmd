757e67acecb5fa8ed2c1cb660da0cd307e377efb
Birger Skogeng Pedersen <birger.sp@gmail.com>
2017-02-21 15:34:10 +0100

    Removed mist submodule, closing #50

--- batch/make.cmd
+++ batch/make.cmd
@@ -18,10 +18,8 @@ unit-types\unit-types.lua
 set /p version=<version.txt
 
 set output_dir=build
-set sources_standalone=%sources% mist\mist_4_3_74.lua
 
 set build=%output_dir%\autogft-%version%.lua
-set build_standalone=%output_dir%\autogft-%version%-standalone.lua
 set comment_prefix=--
 
 set current_dir=%cd%
@@ -33,8 +31,8 @@ del /Q %output_dir%
 rem set input=%sources%
 rem set output=%build%
 rem call:make
-set input=%sources_standalone%
-set output=%build_standalone%
+set input=%sources%
+set output=%build%
 call:make
 
 cd %current_dir%
