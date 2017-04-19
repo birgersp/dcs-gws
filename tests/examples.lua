0febd7273f806646edca577962de552432c0d64d
Birger <birgersp@gmail.com>
2017-04-19 18:16:57 +0200

    Changed examples dir in tests.lua, fixed unit-type reference in example

--- tests/examples.lua
+++ tests/examples.lua
@@ -1,14 +1,14 @@
 
 if not initialized then
 
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\build\autogft-1_9-beta.lua]]))()
+  assert(loadfile([[C:\Users\Birger\Workspace\dcs-autogft\build\autogft-1_9-beta.lua]]))()
   autogft.debugMode = true
 
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\examples\basic.lua]]))()
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\examples\miscellaneous.lua]]))()
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\examples\reinforcing.lua]]))()
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\examples\scanning.lua]]))()
-  assert(loadfile([[C:\Users\birge\Workspace\dcs-autogft\examples\using-roads.lua]]))()
+  assert(loadfile([[C:\Users\Birger\Workspace\dcs-autogft\examples\basic.lua]]))()
+  assert(loadfile([[C:\Users\Birger\Workspace\dcs-autogft\examples\miscellaneous.lua]]))()
+  assert(loadfile([[C:\Users\Birger\Workspace\dcs-autogft\examples\reinforcing.lua]]))()
+  assert(loadfile([[C:\Users\Birger\Workspace\dcs-autogft\examples\scanning.lua]]))()
+  assert(loadfile([[C:\Users\Birger\Workspace\dcs-autogft\examples\using-roads.lua]]))()
 
   initialized = true
 
