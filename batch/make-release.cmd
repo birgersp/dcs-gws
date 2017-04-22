6b347e8866b5cdfdcc0c552695377d52db987b4c
Birger <birgersp@gmail.com>
2017-04-22 10:05:15 +0200

    Removed hardcoded example mission directory

--- batch/make-release.cmd
+++ batch/make-release.cmd
@@ -10,7 +10,7 @@ reinforcing.lua ^
 scanning.lua ^
 test.lua ^
 using-roads.lua
-set example_mission="C:\Users\birge\Saved Games\DCS\Missions\autogft-example.miz"
+set example_mission="%HOMEPATH%\Saved Games\DCS\Missions\autogft-example.miz"
 set build_dir=build
 set examples_destination=%build_dir%\examples
 set archive_dir=build-zip
