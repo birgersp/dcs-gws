5ca5766189d8e9b167053c5f37322d1c2616cb08
Birger <birgersp@gmail.com>
2017-05-14 00:43:46 +0200

    Added example of random units setup

---
+++ examples/random-units.lua
@@ -0,0 +1,14 @@
+
+---
+-- Randomized units example
+-- Using a group of 5 to 10 M-1 Abrams and 2 to 8 M-2 Bradleys
+-- and another group of 0 to 5 AAA Vulcans
+
+autogft_Setup:new()
+  :useRandomUnits()
+  :setCountry(country.id.USA)
+  :addRandomUnitAlternative(10, "M-1 Abrams", 5)
+  :addRandomUnitAlternative(8, "M-2 Bradley", 2)
+  :addGroup()
+  :addRandomUnitAlternative(5, "Vulcan", 0)
+  :addBaseZone("SOME_BASE_NAME")
