730146cfcdd0d2fcc44d49685e6af53dc8a99999
Birger <birgersp@gmail.com>
2017-05-25 00:15:11 +0200

    Added features to unit cluster class

--- autogft/unitcluster.lua
+++ autogft/unitcluster.lua
@@ -19,3 +19,27 @@ function autogft_UnitCluster:new(units, midPoint)
   self.midPoint = midPoint
   return self
 end
+
+---
+-- @param #UnitCluster self
+-- @return #string
+function autogft_UnitCluster:getInfoString()
+  local unitTypes = {}
+  for i = 1, #self.units do
+    local unit = self.units[i]
+    local typeName = unit:getTypeName()
+    if unitTypes[typeName] == nil then
+      unitTypes[typeName] = 0
+    end
+    unitTypes[typeName] = unitTypes[typeName] + 1
+  end
+
+  local text = ""
+  for key, val in pairs(unitTypes) do
+    if text ~= "" then
+      text = text..", "
+    end
+    text = text..val.." "..key
+  end
+  return text
+end
