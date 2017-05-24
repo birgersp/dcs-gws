56cad111fe89929ebf9ad990d91f12bdd87f32cb
Birger <birgersp@gmail.com>
2017-05-24 19:06:47 +0200

    Split observer intel target function into several functions

--- autogft/observerintel.lua
+++ autogft/observerintel.lua
@@ -5,75 +5,11 @@ autogft_observerIntel = {}
 autogft_observerIntel.RE_ENABLING_LOOP_DELAY = 10
 
 ---
--- @type ObserverIntel
--- @extends class#Class
--- @field DCSGroup#Group targetGroup
--- @field #number groupID
--- @field #number enemyCoalitionID
-autogft_ObserverIntel = autogft_Class:create()
+-- @param #list<DCSUnit#Unit> targetUnits
+-- @param #number adjacentUnitThreshold
+function autogft_observerIntel.getUnitClusters(targetUnits, adjacentUnitThreshold)
 
-autogft_ObserverIntel.THREAT_COMMAND_TEXT = "THREAT"
-autogft_ObserverIntel.OBSERVER_MAX_DISTANCE_M = 18500
-autogft_ObserverIntel.UNIT_GROUP_MAX_DISTANCE_M = 800
-autogft_ObserverIntel.MESSAGE_TIME = 60
-
----
--- @param #ObserverIntel self
--- @param DCSGroup#Group targetGroup
--- @return #ObserverIntel
-function autogft_ObserverIntel:new(targetGroup)
-  self = self:createInstance()
-  self.targetGroup = targetGroup
-  self.groupID = targetGroup:getID()
-
-  self.enemyCoalitionID = coalition.side.RED
-  if self.targetGroup:getCoalition() == coalition.side.RED then
-    self.enemyCoalitionID = coalition.side.BLUE
-  end
-
-  local function viewThreat()
-    self:viewThreat()
-  end
-
-  autogft_GroupCommand:new(targetGroup, autogft_ObserverIntel.THREAT_COMMAND_TEXT, viewThreat):enable()
-
-  return self
-end
-
----
--- @param #ObserverIntel self
-function autogft_ObserverIntel:viewThreat()
-
-  local enemyGroundGroups = coalition.getGroups(self.enemyCoalitionID)
-  local observerMaxDistance2 = self.OBSERVER_MAX_DISTANCE_M^2
-
-  local observerPosition = self.targetGroup:getUnit(1):getPosition().p
-  local observerPosVec2 = autogft_Vector2:new(observerPosition.x, observerPosition.z)
-  local observerHeadingNortCorrection = autogft.getHeadingNorthCorrection(observerPosition)
-
-  local unitGroupMaxDistance2 = autogft_ObserverIntel.UNIT_GROUP_MAX_DISTANCE_M^2
-
-  -- Create list of enemy units within max distance
-  local availableEnemyUnits = {}
-  for enemyGroupI = 1, #enemyGroundGroups do
-    local enemyGroup = enemyGroundGroups[enemyGroupI]
-    if enemyGroup and enemyGroup:isExist() then
-      local enemyUnits = enemyGroup:getUnits()
-      for enemyUnitI = 1, #enemyUnits do
-        local enemyUnit = enemyUnits[enemyUnitI] --DCSUnit#Unit
-        if enemyUnit and enemyUnit:isExist() then
-          local enemyPos = enemyUnit:getPosition().p
-          local dX = enemyPos.x - observerPosition.x
-          local dY = enemyPos.y - observerPosition.y
-          local dZ = enemyPos.z - observerPosition.z
-          local distance2 = dX*dX + dY*dY + dZ*dZ
-          if distance2 <= observerMaxDistance2 then
-            availableEnemyUnits[#availableEnemyUnits + 1] = enemyUnit
-          end
-        end
-      end
-    end
-  end
+  local adjacentUnitThreshold2 = adjacentUnitThreshold^2
 
   ---
   -- Creates a cluster of units, from all units adjacent to an origin unit
@@ -104,8 +40,8 @@ function autogft_ObserverIntel:viewThreat()
     -- @param DCSUnit#Unit unit
     local function vehiclesWithinRecurse(targetUnit)
       local targetUnitPos = targetUnit:getPosition().p
-      for i = 1, #availableEnemyUnits do
-        local friendlyGroundUnit = availableEnemyUnits[i] --DCSUnit#Unit
+      for i = 1, #targetUnits do
+        local friendlyGroundUnit = targetUnits[i] --DCSUnit#Unit
 
         -- If not previously added
         if not unitsWithinRangeNames[friendlyGroundUnit:getName()] then
@@ -120,7 +56,7 @@ function autogft_ObserverIntel:viewThreat()
             local dY = unitPos.y - targetUnitPos.y
             local dZ = unitPos.z - targetUnitPos.z
             local distance2 = dX*dX + dY*dY + dZ*dZ
-            if distance2 <= unitGroupMaxDistance2 then
+            if distance2 <= adjacentUnitThreshold2 then
               addUnit(friendlyGroundUnit)
               vehiclesWithinRecurse(friendlyGroundUnit)
             end
@@ -132,12 +68,12 @@ function autogft_ObserverIntel:viewThreat()
 
     -- Units put into the cluster are removed from the list of available enemy units
     local newAvailableUnits = {}
-    for i = 1, #availableEnemyUnits do
-      if not unitsWithinRangeNames[availableEnemyUnits[i]:getName()] then
-        newAvailableUnits[#newAvailableUnits + 1] = availableEnemyUnits[i]
+    for i = 1, #targetUnits do
+      if not unitsWithinRangeNames[targetUnits[i]:getName()] then
+        newAvailableUnits[#newAvailableUnits + 1] = targetUnits[i]
       end
     end
-    availableEnemyUnits = newAvailableUnits
+    targetUnits = newAvailableUnits
 
     -- Determine cluster midpoint
     local dx = maxPos.x - minPos.x
@@ -149,48 +85,137 @@ function autogft_ObserverIntel:viewThreat()
 
   -- Create clusters from all available enemy units
   local clusters = {}
-  while #availableEnemyUnits > 0 do
-    local cluster = getCluster(availableEnemyUnits[1])
+  while #targetUnits > 0 do
+    local cluster = getCluster(targetUnits[1])
     clusters[#clusters + 1] = cluster
   end
 
+  return clusters
+end
+
+---
+-- @param DCSVec3#Vec3 observerPosition
+-- @param #list<DCSUnit#Unit> targetUnits
+-- @param #number adjacentUnitThreshold
+function autogft_observerIntel.getTargetUnitsMessage(observerPosition, targetUnits, adjacentUnitThreshold)
+
+  local observerPosVec2 = autogft_Vector2:new(observerPosition.x, observerPosition.z)
+  local observerHeadingNortCorrection = autogft.getHeadingNorthCorrection(observerPosition)
+
+  local clusters = autogft_observerIntel.getUnitClusters(targetUnits, adjacentUnitThreshold)
+
   -- Create message from clusters
   local message = ""
-  if #clusters == 0 then
-    message = "no enemy units observed"
-  else
-    for clusterI = 1, #clusters do
-      local cluster = clusters[clusterI] --unitcluster#UnitCluster
-      local unitTypes = {}
-      for i = 1, #cluster.units do
-        local unit = cluster.units[i]
-        local typeName = unit:getTypeName()
-        if unitTypes[typeName] == nil then
-          unitTypes[typeName] = 0
-        end
-        unitTypes[typeName] = unitTypes[typeName] + 1
+  for clusterI = 1, #clusters do
+    local cluster = clusters[clusterI] --unitcluster#UnitCluster
+    local unitTypes = {}
+    for i = 1, #cluster.units do
+      local unit = cluster.units[i]
+      local typeName = unit:getTypeName()
+      if unitTypes[typeName] == nil then
+        unitTypes[typeName] = 0
       end
-
-      local text = ""
-      for key, val in pairs(unitTypes) do
-        if text ~= "" then
-          text = text..", "
-        end
-        text = text..val.." "..key
-        autogft.log(text)
-      end
-
-      local observerToCluster = autogft_Vector2.minus(cluster.midPoint, observerPosVec2)
-      local dirRad = autogft_Vector2.Axis.X:getAngleTo(observerToCluster) + observerHeadingNortCorrection
-      local dirHeading = math.floor(dirRad / math.pi * 180 + 0.5)
-      local distanceM = observerToCluster:getMagnitude()
-      local distanceKM = distanceM / 1000
-      local distanceNM = distanceKM / 1.852
-
-      local distanceNMRounded = math.floor(distanceNM + 0.5)
-      text = text .. " located " .. distanceNMRounded .. "nm at " .. dirHeading
-      message = message .. text .. "\n"
+      unitTypes[typeName] = unitTypes[typeName] + 1
     end
+
+    local text = ""
+    for key, val in pairs(unitTypes) do
+      if text ~= "" then
+        text = text..", "
+      end
+      text = text..val.." "..key
+      autogft.log(text)
+    end
+
+    local observerToCluster = autogft_Vector2.minus(cluster.midPoint, observerPosVec2)
+    local dirRad = autogft_Vector2.Axis.X:getAngleTo(observerToCluster) + observerHeadingNortCorrection
+    local dirHeading = math.floor(dirRad / math.pi * 180 + 0.5)
+    local distanceM = observerToCluster:getMagnitude()
+    local distanceKM = distanceM / 1000
+    local distanceNM = distanceKM / 1.852
+
+    local distanceNMRounded = math.floor(distanceNM + 0.5)
+    text = text .. " located " .. distanceNMRounded .. "nm at " .. dirHeading
+    message = message .. text .. "\n"
+  end
+  return message
+end
+
+---
+-- @type ObserverIntel
+-- @extends class#Class
+-- @field DCSGroup#Group targetGroup
+-- @field #number groupID
+-- @field #number enemyCoalitionID
+autogft_ObserverIntel = autogft_Class:create()
+
+autogft_ObserverIntel.TARGET_COMMAND_TEXT = "TARGET"
+autogft_ObserverIntel.OBSERVER_MAX_DISTANCE_M = 18500
+autogft_ObserverIntel.UNIT_GROUP_MAX_DISTANCE_M = 800
+autogft_ObserverIntel.MESSAGE_TIME = 60
+autogft_ObserverIntel.NO_TARGETS_OBSERVED_MESSAGE = "No targets observed"
+
+---
+-- @param #ObserverIntel self
+-- @param DCSGroup#Group targetGroup
+-- @return #ObserverIntel
+function autogft_ObserverIntel:new(targetGroup)
+  self = self:createInstance()
+  self.targetGroup = targetGroup
+  self.groupID = targetGroup:getID()
+
+  self.enemyCoalitionID = coalition.side.RED
+  if self.targetGroup:getCoalition() == coalition.side.RED then
+    self.enemyCoalitionID = coalition.side.BLUE
+  end
+
+  local function viewTarget()
+    self:viewTarget()
+  end
+
+  autogft_GroupCommand:new(targetGroup, autogft_ObserverIntel.TARGET_COMMAND_TEXT, viewTarget):enable()
+
+  return self
+end
+
+---
+-- @param #ObserverIntel self
+function autogft_ObserverIntel:viewTarget()
+
+  local enemyGroundGroups = coalition.getGroups(self.enemyCoalitionID)
+  local observerMaxDistance2 = self.OBSERVER_MAX_DISTANCE_M^2
+
+  local observerPosition = self.targetGroup:getUnit(1):getPosition().p
+
+  local unitGroupMaxDistance2 = autogft_ObserverIntel.UNIT_GROUP_MAX_DISTANCE_M^2
+
+  -- Create list of enemy units within max distance
+  local availableEnemyUnits = {}
+  for enemyGroupI = 1, #enemyGroundGroups do
+    local enemyGroup = enemyGroundGroups[enemyGroupI]
+    if enemyGroup and enemyGroup:isExist() then
+      local enemyUnits = enemyGroup:getUnits()
+      for enemyUnitI = 1, #enemyUnits do
+        local enemyUnit = enemyUnits[enemyUnitI] --DCSUnit#Unit
+        if enemyUnit and enemyUnit:isExist() then
+          local enemyPos = enemyUnit:getPosition().p
+          local dX = enemyPos.x - observerPosition.x
+          local dY = enemyPos.y - observerPosition.y
+          local dZ = enemyPos.z - observerPosition.z
+          local distance2 = dX*dX + dY*dY + dZ*dZ
+          if distance2 <= observerMaxDistance2 then
+            availableEnemyUnits[#availableEnemyUnits + 1] = enemyUnit
+          end
+        end
+      end
+    end
+  end
+
+  local message
+  if (#availableEnemyUnits == 0) then
+    message = autogft_ObserverIntel.NO_TARGETS_OBSERVED_MESSAGE
+  else
+    message = autogft_observerIntel.getTargetUnitsMessage(observerPosition, availableEnemyUnits, autogft_ObserverIntel.UNIT_GROUP_MAX_DISTANCE_M)
   end
 
   trigger.action.outTextForGroup(self.groupID, message, autogft_ObserverIntel.MESSAGE_TIME)
