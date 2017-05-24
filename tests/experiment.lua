4337ef2c419f74b8d6aa3013a23e8a410024042c
Birger <birgersp@gmail.com>
2017-05-24 22:37:06 +0200

    Updated experiment

--- tests/experiment.lua
+++ tests/experiment.lua
@@ -3,7 +3,27 @@ if not initialized then
 
   dofile("C:\\Users\\Birger\\Workspace\\dcs-autogft\\tests\\load-all.lua")
 
-  autogft_observerIntel.enable("JTAC")
+  autogft_Setup:new()
+    :addBaseZone("MAIN_BASE")
+    :addControlZone("F_BASE1")
+    :copyGroupsLayout("GROUP1")
+
+  autogft_Setup:new()
+    :addBaseZone("MAIN_BASE")
+    :addControlZone("F_BASE2")
+    :copyGroupsLayout("GROUP2")
+
+  autogft_Setup:new()
+    :useStaging()
+    :addBaseZone("F_BASE1")
+    :addControlZone("COMBAT")
+    :scanUnits("GROUP1")
+
+  autogft_Setup:new()
+    :useStaging()
+    :addBaseZone("F_BASE2")
+    :addControlZone("COMBAT")
+    :scanUnits("GROUP2")
 
   initialized = true
 
