a0f272d80fa2e8c10bfda612106014f8b64347bd
Birger Skogeng Pedersen <birgersp@gmail.com>
2019-03-18 18:07:52 +0100

    Added unit types

---
+++ unit-types.lua
@@ -0,0 +1,209 @@
+unitType = {}
+
+unitType.navy = {}
+
+unitType.navy.blue = {
+  VINSON = "VINSON",
+  PERRY = "PERRY",
+  TICONDEROG = "TICONDEROG"
+}
+
+unitType.navy.red = {
+  ALBATROS = "ALBATROS",
+  KUZNECOW = "KUZNECOW",
+  MOLNIYA = "MOLNIYA",
+  MOSCOW = "MOSCOW",
+  NEUSTRASH = "NEUSTRASH",
+  PIOTR = "PIOTR",
+  REZKY = "REZKY"
+}
+
+unitType.navy.civil = {
+  ELNYA = "ELNYA",
+  Drycargo_ship2 = "Dry-cargo ship-2",
+  Drycargo_ship1 = "Dry-cargo ship-1",
+  ZWEZDNY = "ZWEZDNY"
+}
+
+unitType.navy.submarine = {
+  KILO = "KILO",
+  SOM = "SOM"
+}
+
+unitType.navy.speedboat = {
+  speedboat = "speedboat"
+}
+
+unitType.infantry = {
+  Soldier_AK = "Soldier AK",
+  Infantry_AK = "Infantry AK",
+  Soldier_M249 = "Soldier M249",
+  Soldier_M4 = "Soldier M4",
+  Soldier_M4_GRG = "Soldier M4 GRG",
+  Soldier_RPG = "Soldier RPG",
+  Paratrooper_RPG16 = "Paratrooper RPG-16",
+  Paratrooper_AKS74 = "Paratrooper AKS-74"
+}
+
+unitType.vehicle = {}
+
+unitType.vehicle.howitzer = {
+  _2B11_mortar = "2B11 mortar",
+  SAU_Gvozdika = "SAU Gvozdika",
+  SAU_Msta = "SAU Msta",
+  SAU_Akatsia = "SAU Akatsia",
+  SAU_2C9 = "SAU 2-C9",
+  M109 = "M-109"
+}
+
+unitType.vehicle.ifv = {
+  AAV7 = "AAV7",
+  BMD1 = "BMD-1",
+  BMP1 = "BMP-1",
+  BMP2 = "BMP-2",
+  BMP3 = "BMP-3",
+  Boman = "Boman",
+  BRDM2 = "BRDM-2",
+  BTR80 = "BTR-80",
+  BTR_D = "BTR_D",
+  Bunker = "Bunker",
+  Cobra = "Cobra",
+  LAV25 = "LAV-25",
+  M1043_HMMWV_Armament = "M1043 HMMWV Armament",
+  M1045_HMMWV_TOW = "M1045 HMMWV TOW",
+  M1126_Stryker_ICV = "M1126 Stryker ICV",
+  M113 = "M-113",
+  M1134_Stryker_ATGM = "M1134 Stryker ATGM",
+  M2_Bradley = "M-2 Bradley",
+  Marder = "Marder",
+  MCV80 = "MCV-80",
+  MTLB = "MTLB",
+  Sandbox = "Sandbox",
+  TPZ = "TPZ"
+}
+
+unitType.vehicle.mlrs = {
+  GradURAL = "Grad-URAL",
+  Uragan_BM27 = "Uragan_BM-27",
+  Smerch = "Smerch",
+  MLRS = "MLRS"
+}
+
+unitType.vehicle.sam = {
+  _2S6_Tunguska = "2S6 Tunguska",
+  Kub_2P25_ln = "Kub 2P25 ln",
+  _5p73_s125_ln = "5p73 s-125 ln",
+  S300PS_5P85C_ln = "S-300PS 5P85C ln",
+  S300PS_5P85D_ln = "S-300PS 5P85D ln",
+  SA11_Buk_LN_9A310M1 = "SA-11 Buk LN 9A310M1",
+  Osa_9A33_ln = "Osa 9A33 ln",
+  Tor_9A331 = "Tor 9A331",
+  Strela10M3 = "Strela-10M3",
+  Strela1_9P31 = "Strela-1 9P31",
+  SA11_Buk_CC_9S470M1 = "SA-11 Buk CC 9S470M1",
+  SA8_Osa_LD_9T217 = "SA-8 Osa LD 9T217",
+  Patriot_AMG = "Patriot AMG",
+  Patriot_ECS = "Patriot ECS",
+  Gepard = "Gepard",
+  Hawk_pcp = "Hawk pcp",
+  SA18_Igla_manpad = "SA-18 Igla manpad",
+  SA18_Igla_comm = "SA-18 Igla comm",
+  Igla_manpad_INS = "Igla manpad INS",
+  SA18_IglaS_manpad = "SA-18 Igla-S manpad",
+  SA18_IglaS_comm = "SA-18 Igla-S comm",
+  Vulcan = "Vulcan",
+  Hawk_ln = "Hawk ln",
+  M48_Chaparral = "M48 Chaparral",
+  M6_Linebacker = "M6 Linebacker",
+  Patriot_ln = "Patriot ln",
+  M1097_Avenger = "M1097 Avenger",
+  Patriot_EPP = "Patriot EPP",
+  Patriot_cp = "Patriot cp",
+  Roland_ADS = "Roland ADS",
+  S300PS_54K6_cp = "S-300PS 54K6 cp",
+  Stinger_manpad_GRG = "Stinger manpad GRG",
+  Stinger_manpad_dsr = "Stinger manpad dsr",
+  Stinger_comm_dsr = "Stinger comm dsr",
+  Stinger_manpad = "Stinger manpad",
+  Stinger_comm = "Stinger comm",
+  ZSU234_Shilka = "ZSU-23-4 Shilka",
+  ZU23_Emplacement_Closed = "ZU-23 Emplacement Closed",
+  ZU23_Emplacement = "ZU-23 Emplacement",
+  ZU23_Closed_Insurgent = "ZU-23 Closed Insurgent",
+  Ural375_ZU23_Insurgent = "Ural-375 ZU-23 Insurgent",
+  ZU23_Insurgent = "ZU-23 Insurgent",
+  Ural375_ZU23 = "Ural-375 ZU-23"
+}
+
+unitType.vehicle.radar = {
+  _1L13_EWR = "1L13 EWR",
+  Kub_1S91_str = "Kub 1S91 str",
+  S300PS_40B6M_tr = "S-300PS 40B6M tr",
+  S300PS_40B6MD_sr = "S-300PS 40B6MD sr",
+  _55G6_EWR = "55G6 EWR",
+  S300PS_64H6E_sr = "S-300PS 64H6E sr",
+  SA11_Buk_SR_9S18M1 = "SA-11 Buk SR 9S18M1",
+  Dog_Ear_radar = "Dog Ear radar",
+  Hawk_tr = "Hawk tr",
+  Hawk_sr = "Hawk sr",
+  Patriot_str = "Patriot str",
+  Hawk_cwar = "Hawk cwar",
+  p19_s125_sr = "p-19 s-125 sr",
+  Roland_Radar = "Roland Radar",
+  snr_s125_tr = "snr s-125 tr"
+}
+
+unitType.vehicle.structure = {
+  house1arm = "house1arm",
+  house2arm = "house2arm",
+  outpost_road = "outpost_road",
+  outpost = "outpost",
+  houseA_arm = "houseA_arm"
+}
+
+unitType.vehicle.tank = {
+  Challenger2 = "Challenger2",
+  Leclerc = "Leclerc",
+  Leopard1A3 = "Leopard1A3",
+  Leopard2 = "Leopard-2",
+  M60 = "M-60",
+  M1128_Stryker_MGS = "M1128 Stryker MGS",
+  M1_Abrams = "M-1 Abrams",
+  T55 = "T-55",
+  T72B = "T-72B",
+  T80UD = "T-80UD",
+  T90 = "T-90"
+}
+
+unitType.vehicle.unarmed = {
+  Ural4320_APA5D = "Ural-4320 APA-5D",
+  ATMZ5 = "ATMZ-5",
+  ATZ10 = "ATZ-10",
+  GAZ3307 = "GAZ-3307",
+  GAZ3308 = "GAZ-3308",
+  GAZ66 = "GAZ-66",
+  M978_HEMTT_Tanker = "M978 HEMTT Tanker",
+  HEMTT_TFFT = "HEMTT TFFT",
+  IKARUS_Bus = "IKARUS Bus",
+  KAMAZ_Truck = "KAMAZ Truck",
+  LAZ_Bus = "LAZ Bus",
+  Hummer = "Hummer",
+  M_818 = "M 818",
+  MAZ6303 = "MAZ-6303",
+  Predator_GCS = "Predator GCS",
+  Predator_TrojanSpirit = "Predator TrojanSpirit",
+  Suidae = "Suidae",
+  Tigr_233036 = "Tigr_233036",
+  Trolley_bus = "Trolley bus",
+  UAZ469 = "UAZ-469",
+  Ural_ATsP6 = "Ural ATsP-6",
+  Ural375_PBU = "Ural-375 PBU",
+  Ural375 = "Ural-375",
+  Ural432031 = "Ural-4320-31",
+  Ural4320T = "Ural-4320T",
+  VAZ_Car = "VAZ Car",
+  ZiL131_APA80 = "ZiL-131 APA-80",
+  SKP11 = "SKP-11",
+  ZIL131_KUNG = "ZIL-131 KUNG",
+  ZIL4331 = "ZIL-4331"
+}
