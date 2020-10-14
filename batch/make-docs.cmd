f36047ce5e8dc090dce07a6eee536271701c7c59
Birger Skogeng Pedersen <birger.sp@gmail.com>
2020-10-14 20:14:59 +0200

    impl new doc compiler

--- batch/make-docs.cmd
+++ batch/make-docs.cmd
@@ -1,9 +1,2 @@
-rem remove this once a new lua documentor is ready
-echo Skipping doc generator
-exit /b 2
-
-set current_dir=%cd%
-cd ..
 del /Q docs\*
-call luadocumentor.bat autogft
-cd %current_dir%
+ldcapp inDir=autogft outDir=docs
