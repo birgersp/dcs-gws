eb2bd7cc5ed4ea6ae9e6e94262da708fb03172f6
Birger Skogeng Pedersen <birger.sp@gmail.com>
2020-11-23 21:13:52 +0100

    experiment

--- tests/experiment.lua
+++ tests/experiment.lua
@@ -1,5 +1,32 @@
-autogft_Setup:new()
-:addBaseZone("BLUE_BASE")
-:setReinforceTimerMax(1200)
-:addControlZone("CONTROL1")
-:addControlZone("CONTROL2")
+--autogft_Setup:new()
+--  :addBaseZone("BLUE_BASE")
+--  :addControlZone("CONTROL1")
+
+
+local function go()
+  local groups = coalition.getGroups(coalition.side.BLUE)
+  autogft.log(groups)
+  local group = groups[1]
+  local dcsTask = {
+    id = "Mission",
+    params = {
+      route = {
+        points = {
+          [1] = {
+            y = 0,
+            x = 0,
+            action = "Cone",
+            speed = 10,
+            type = "Turning Point"
+          }
+        }
+      }
+    }
+  }
+  autogft.log(group)
+  local controller = group:getController()
+  autogft.log(controller)
+  controller:setTask(dcsTask)
+end
+
+autogft.scheduleFunction(go, 3)
