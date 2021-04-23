5ff40c45db9453cafb113ab4d51785f2981cb1b9
Birger Skogeng Pedersen <birger.sp@gmail.com>
2021-04-23 14:51:52 +0200

    fix make-release script, edit experiment

--- ps/make-release.ps1
+++ ps/make-release.ps1
@@ -6,7 +6,7 @@ $buildDir = "build"
 $version = Get-Content -Path $versionFile
 $archiveFile = "autogft-$($version).zip"
 $examplesDestination = "$($buildDir)\examples"
-$exampleMission="~\Saved Games\DCS\Missions\autogft-example.miz"
+$exampleMission="~\Saved Games\DCS.openbeta\Missions\autogft-example.miz"
 
 Remove-Item .\build\* -Recurse -Force
 & .\ps\make.ps1
--- tests/experiment.lua
+++ tests/experiment.lua
@@ -1,35 +1,4 @@
-local function log(string)
-  autogft.log(string)
-end
-
-local waypoints = {}
-local function addWaypoint(x, y)
-  waypoints[#waypoints + 1] = {
-    x = x,
-    y = y,
-    speed = 20,
-    action = "Cone"
-  }
-end
-
-local function foobar()
-  env.info("WAYPOINT TEST")
-  local group1 = Group.getByName("Group1")
-  local group1Pos = group1:getUnit(1):getPosition().p
-  addWaypoint(group1Pos.x, group1Pos.z)
-  addWaypoint(group1Pos.x + 2500, group1Pos.z)
-  addWaypoint(group1Pos.x + 2500, group1Pos.z + 2500)
-  addWaypoint(group1Pos.x, group1Pos.z + 2500)
-  addWaypoint(group1Pos.x, group1Pos.z)
-  local task = {
-    id = "Mission",
-    params = {
-      route = {
-        points = waypoints
-      }
-    }
-  }
-  log(group1:getController())
-  group1:getController():setTask(task)
-end
-timer.scheduleFunction(foobar,nil,5)
+autogft_Setup:new()
+  :addBaseZone("Zone3")
+  :addControlZone("Zone1")
+  :addControlZone("Zone2")
